60ffef920351215ad5bc2a14cc0e87f6d271e8b0
Angelishere <angelsara1804@gmail.com>
2024-04-08 22:37:53 +0530

    view recipe added

---
+++ src/components/ViewRecipe.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+
+const ViewRecipe = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2023/03/kadhi-pakora.jpg.webp" class="card-img-top" alt="..." />
+                                        <div class="card-body">
+                                        <a href="https://www.indianhealthyrecipes.com/kadhi-pakora-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2022/03/paneer-recipes-swasthi.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/paneer-recipes/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2022/05/rajma-recipe.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/rajma-recipe-rajma-masala-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/12/malpua.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/easy-malpua-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/aloo-chaat.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/aloo-chaat-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/10/kadai-paneer.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/kadai-paneer-gravy-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2021/07/carrot-halwa.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/carrot-halwa-recipe-gajar-ka-halwa-recipe/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                <div class="card">
+                                    <img src="https://www.indianhealthyrecipes.com/wp-content/uploads/2022/06/paneer-tikka-recipe.jpg.webp" class="card-img-top" alt="..."/>
+                                        <div class="card-body">
+                                            <a href="https://www.indianhealthyrecipes.com/paneer-tikka-on-stove-top/" class="card-link">Recipe</a>
+                                        </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewRecipe
